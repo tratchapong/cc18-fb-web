68c851514a6c5c8fb55d0d2256ec1b93ad7fe6ed
tratchapong <tratchapong@gmail.com>
2024-10-01 10:17:42 +0700

    start with tailwind+daisyUI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        CC18 Fakebook
+      </h1>
+      <div className="btn btn-primary">OK</div>
+    </>
+  )
+}
+
+export default App
