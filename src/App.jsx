9acb4aef709ac550cc9b3dbd9122b7750e8e69fa
tratchapong <tratchapong@gmail.com>
2024-10-02 13:52:16 +0700

    start main layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,49 @@
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        CC18 Fakebook
-      </h1>
-      <div className="btn btn-primary">OK</div>
-    </>
+    <div className="min-h-screen bg-yellow-100">
+      <header className="bg-orange-200 h-14 w-full fixed top-0 z-10">Header</header>
+      <main className="relative flex bg-blue-200 border h-screen pt-14">
+        <div className="fixed top-14 h-full bg-red-200 w-[280px] overflow-auto">
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+        </div>
+        <div className="border px-[280px]">
+        <div className="artboard phone-3">414×736</div>
+        <div className="artboard phone-3">414×736</div>
+        <div className="artboard phone-3">414×736</div>
+        <div className="artboard phone-3">414×736</div>
+        </div>
+        <div className="fixed top-14 right-0 h-full bg-red-200 w-[280px] overflow-auto ">
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+          <div className="btn btn-xl m-10">Menu Item</div>
+        </div>
+      </main>
+    </div>
   )
 }
 
