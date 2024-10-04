ee1fc50023d885b645188c9b1a75359709753769
tratchapong <tratchapong@gmail.com>
2024-10-04 10:19:47 +0700

    AppRouter : with children - Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
+import { Outlet } from "react-router-dom"
 import Header from "./components/Header"
-import PostContainer from "./components/PostContainer"
-import SidebarContact from "./components/SidebarContact"
-import SidebarMenu from "./components/SidebarMenu"
-
-
 
 function App() {
-
   return (
     <div className="min-h-screen bg-yellow-100">
       <Header />
       <main className="relative flex bg-gray-100 border pt-14">
-        <SidebarMenu />      
-        <PostContainer />
-        <SidebarContact />
+        <Outlet />
       </main>
     </div>
   )
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,9 @@
 import {createBrowserRouter, RouterProvider, Navigate} from 'react-router-dom'
 import Login from '../pages/Login'
 import App from '../App'
+import SidebarMenu from '../components/SidebarMenu'
+import PostContainer from '../components/PostContainer'
+import SidebarContact from '../components/SidebarContact'
 
 const guestRouter = createBrowserRouter([
 	{ path : '/', element: <Login />},
@@ -8,12 +11,22 @@ const guestRouter = createBrowserRouter([
 ])
 
 const userRouter = createBrowserRouter([
-	{ path : '/', element : <App />},
-	{ path : 'friends', element: <p>Friends</p> }
+	{ path : '/', element : <App />, 
+		children : [
+			{path: '', element :<>
+				<SidebarMenu />
+				<PostContainer />
+				<SidebarContact />
+			</>},
+			{path: 'friends', element : <p>Friends Page</p>},
+			{path: '*', element : <Navigate to='/' />}
+		]
+	},
+	
 ])
 
 export default function AppRouter() {
-	const user = null
+	const user = {}
 	const finalRouter = user ? userRouter : guestRouter
 	return(
 		<RouterProvider router={finalRouter} />
